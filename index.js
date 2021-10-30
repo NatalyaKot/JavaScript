42c45eece6e09a468423da2e2657a766efd3bdc6
John Doe <johndoe@example.com>
2021-10-30 14:16:16 +0300

    Init commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// console.log("Hello World");
+
+// let myName = "Natalya";
+
+// console.log(myName);
+
+
+// let namePlanet = "Venera";
+
+// console.log(namePlanet);
+
+
+// let currentTimeUser = "15:08";
+
+// console.log(currentTimeUser);
+
+
+
+// let counterArticle = "100";
+
+// console.log(counterArticle);
+
+
+
+// let payCardOrCash = "Card";
+
+// console.log(payCardOrCash);
+
+
+
+// let firstName = "Kotkova";
+// let lastName = "Sergeevna";
+// let middleName = "Natalya";
+
+// console.log(firstName, middleName, lastName);
+
+
+// alert( "Я JavaScript!" );
+
+// const myBirthday = '18.04.1982';
+
+
+$ brew install git
